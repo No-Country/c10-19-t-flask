6e31c6fcc4f061979a753b2793a6938d5bd3322f
Emilio <planeta.chompi18@gmail.com>
2023-03-30 14:29:39 +0800

    SingIn for to review

--- app-gestion-finanzas/src/components/NavBar.vue
+++ app-gestion-finanzas/src/components/NavBar.vue
@@ -6,20 +6,23 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary col-md-12">
-    <div class="container-fluid">
-      
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <RouterLink to="/" class="nav-link active" aria-current="page">Home</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink to="/about" class="nav-link active" aria-current="page">About</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink to="/singin" class="nav-link active" aria-current="page">Sing in</RouterLink>
-        </li>
-      </ul>
+    <div class="container-fluid text-center">
+      <div class="row">
+        <div class="">
+          <div class="collapse navbar-collapse d-flex p-2" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink to="/" class="nav-link active" aria-current="page">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/about" class="nav-link active" aria-current="page">About</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/singin" class="nav-link active" aria-current="page">Sing in</RouterLink>
+              </li>
+            </ul>
+          </div>
+        </div>
     </div>
   </div>
 </nav>
