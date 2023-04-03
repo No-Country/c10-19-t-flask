6ddde1064c7730b8ef7da207c896d317b36f97db
Emilio <planeta.chompi18@gmail.com>
2023-04-04 07:10:04 +0800

    added totals

--- app-gestion-finanzas/src/views/TransactionsView.vue
+++ app-gestion-finanzas/src/views/TransactionsView.vue
@@ -17,11 +17,25 @@ import {RouterLink} from 'vue-router'
     
  
   
-<div class="container">
-<div class="row">
-    <div class="">
-        <h1>soy transactions</h1>
+<div class="container-fluid">
+<div class="row d-flex justify-content-evenly p-2">
+    <div class="col-12 text-center rounded-5 bg-primary p-3">
+      <h1>Total</h1>
+      <h2>$12000</h2>
     </div>
+    <div class="col-6 text-center rounded-5 bg-danger border p-3">
+        <Text class="fs-3">persona-1</text><br/>
+        <text class="fs-3 fw-medium">$100000000</text>
+    </div>
+    <div class="col-6 text-center rounded-5 bg-warning border p-3">
+      <text class="fs-3">persona 2</text><br/>
+      <h2 class="fs-3 fw-medium">$2000</h2>
+    </div>
+</div>
+<div class="row justify-content-center mt-4">
+  <div class="col text-center">
+    <h1>Today</h1>
+  </div>
 </div>
 </div>
 <footer>
