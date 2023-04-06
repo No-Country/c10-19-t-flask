7bce9c96451c373bd4e27e864208d94d2e5d4973
Emilio <planeta.chompi18@gmail.com>
2023-04-07 07:13:58 +0800

    AddCategoryView and CategoriesView finished

--- app-gestion-finanzas/src/views/CategoriesView.vue
+++ app-gestion-finanzas/src/views/CategoriesView.vue
@@ -14,27 +14,46 @@ import {RouterLink} from 'vue-router'
   <AsideNavBar />
 
 <div class="container">
+
     <div class="row d-flex aling-items-center justify-content-center mt-4">
+
       <div class="col-12 col-lg-9 py-5 shadow-lg bg-light mb-5 h-50">
-      <div class="col-12 text-center p-2 shadow">
-          <h1>Categories</h1>
+
+        <div class="col-12 text-center p-2 shadow">
+            <h1>Categories</h1>
+        </div>
+
+        <div class="row row-cols-lg-4 row-cols-3 p-3 justify-content-around text-center fs-4 fw-semibold">
+          <div class="col p-3 ">
+            <p class="text-wrap">House<br><i class="bi bi-house-fill p-1"></i>$ 20000</p>
+          </div>
+
+          <div class="col p-3">
+            <p class="text-wrap">Pet<br><i class="bi bi-house-fill p-1"></i><br>$ 50</p>
+          </div>
+    
+          <div class="col p-3 ">
+            <p class="text-wrap">Gifts<br><i class="bi bi-house-fill p-1"></i><br>$ 300</p>
+          </div>
+
+          <div class="col p-3 ">
+            <p class="text-wrap">Bills<br><i class="bi bi-house-fill p-1"></i><br>$ 250</p>
+          </div>
+
+          <div class="col p-3 ">
+            <p class="text-wrap">Grosseries<br><i class="bi bi-house-fill p-1"></i><br>$ 1000</p>
+          </div>
+
+          <div class="col p-3">
+            <RouterLink to="/addcategory"><p class="text-center mt-4 text-secondary" style="font-size: 2.1rem;"><i class="bi bi-plus-circle-fill"></i></p></RouterLink>
+          </div>
+
+        </div>
+    
+       </div>
+
     </div>
 
-   <div class="row row-cols-lg-4 row-cols-3 p-3 justify-content-around text-center fs-4 fw-semibold">
-    <div class="col p-3 ">
-      <p class="text-wrap">House<br><i class="bi bi-house-fill p-1"></i>$20</p>
-      
-    </div>
-    
-    <div class="col p-3">Column</div>
-    <div class="col p-3">Column</div>
-    <div class="col p-3">Column</div>
-    <div class="col p-3">Column</div>
-   </div>
-    
-    
-    </div>
-  </div>
   </div>
 
 
