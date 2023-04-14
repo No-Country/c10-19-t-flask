ef02b21c495d35bcc3b9d5ea01cf961034d39e2c
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2023-04-14 00:18:43 -0300

    changes prod config

--- api/config/prod.py
+++ api/config/prod.py
@@ -8,6 +8,6 @@ bd_type = "mysql+pymysql"
 host = "localhost"
 
 SQLALCHEMY_DATABASE_URI = f'{bd_type}://{usuario}:{contrasenia}@{host}/{nombre_bd}'
-
+print(SQLALCHEMY_DATABASE_URI)
 APP_ENV = APP_ENV_LOCAL
 DEBUG = False
