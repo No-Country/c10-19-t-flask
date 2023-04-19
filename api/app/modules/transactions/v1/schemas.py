3e491a38f2e701ddd9348f5adb4f046f54996927
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2023-04-19 01:43:32 -0300

    schema date

--- api/app/modules/transactions/v1/schemas.py
+++ api/app/modules/transactions/v1/schemas.py
@@ -2,6 +2,6 @@ from marshmallow import EXCLUDE, Schema, fields
 
 class TransactionSchema(Schema):
     class Meta:
-        additional = ('id','type','value','category_id','currency_id','group_id')
+        additional = ('id','type','value','category_id','currency_id','group_id', 'date')
         unknown = EXCLUDE
         ordered = True
