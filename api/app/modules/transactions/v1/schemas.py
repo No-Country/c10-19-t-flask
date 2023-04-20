53d876ca33edd8b0a4022450103c928f7640f81b
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2023-04-20 11:17:57 -0300

    correcion schema transaction

--- api/app/modules/transactions/v1/schemas.py
+++ api/app/modules/transactions/v1/schemas.py
@@ -6,6 +6,6 @@ from app.modules.members.v1.schemas import UserSchema
 class TransactionSchema(Schema):
     user = fields.Nested(UserSchema, only=('external_id', "fullname"))
     class Meta:
-        additional = ('id','type','value','category_id','currency_id','group_id', 'date')
+        additional = ('id','type','value','category_id','currency_id','group_id', 'date', 'user_id')
         unknown = EXCLUDE
         ordered = True
