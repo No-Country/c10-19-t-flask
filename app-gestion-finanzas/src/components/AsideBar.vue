befddf88404f148fb1ea6e70fc7a6fbe39254657
Emilio <planeta.chompi18@gmail.com>
2023-04-21 06:01:38 +0800

    adding groups button

--- app-gestion-finanzas/src/components/AsideBar.vue
+++ app-gestion-finanzas/src/components/AsideBar.vue
@@ -17,11 +17,12 @@ import { RouterLink, RouterView } from 'vue-router'
                     <text><i class="bi bi-plus-circle-fill text-success p-2" ></i>Add expending</text>
                 </li></RouterLink>
  
-                <!-- <li id="calendar">
-                    <button type="button" class="btn link-opacity-50-hover">Mostrar calendario</button>
+                <li id="calendar">
+                    <ul class="navbar-nav me-auto fs-5 fw-semibold text-center ">Grupos</ul>
+                    <li class="p-3 fs-6">Grupo 1</li>
                 </li>
 
-                <RouterLink to="/addnewgroup" class="nav-link active link-opacity-50-hover" aria-current="page"><li class="p-3 text-center fs-5">
+                <!--<RouterLink to="/addnewgroup" class="nav-link active link-opacity-50-hover" aria-current="page"><li class="p-3 text-center fs-5">
                     <text><i class="bi bi-folder-plus text-success p-2 "></i>Start a New Group</text>
                 </li></RouterLink> -->
 
@@ -30,14 +31,23 @@ import { RouterLink, RouterView } from 'vue-router'
     </aside>
 
     <aside class="p-1 d-md-none d-sm-block bg-light shadow border border-start-0 d-inline-block p3 vw-100" >
-        <!-- <div class="container-fluid">
-        <ul class="navbar-nav me-auto d-flex flex-row justify-content-between p-3">
-                <li >Months</li>
+        <div class="container-fluid">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Groups
+  </button>
+        <ul class="navbar-nav me-auto d-flex flex-row justify-content-between p-3 dropdown-menu">
+                <!--<li >Months</li>
                 <RouterLink to="/addnewgroup" class="nav-link active" aria-current="page">
                     <li>New Group</li>
-                </RouterLink>
-            </ul>
-        </div> -->
+                </RouterLink>-->
+        
+    
+            <li class="p-3 fs-6">Grupo 1</li>
+        
+        </ul>
+        </div>
+        </div>
     </aside>
 
 </template>
